c6d5a33e2ff11e0d39f6133d6f33ccb77763f243
Nirmani-Rathnayaka <nirmaninipunika053@gmail.com>
2020-11-13 15:35:49 +0530

    ceate models

---
+++ SP_API/Models/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SP_API.Models
+{
+    public class Client
+    {
+        
+        public int client_ID { get; set; }
+        public string company_name { get; set; }
+        public int tele_no { get; set; }
+        public string company_title { get; set; }
+        public string email { get; set; }
+    }
+}
