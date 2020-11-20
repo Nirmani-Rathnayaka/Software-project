995ce76183b6e6d94d97b92b306391606042a90a
TharindaUD <tharindadharmasena@gmail.com>
2020-11-20 14:31:35 +0530

    create models

---
+++ SP_API/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SP_API.Models
+{
+	class Admin
+	{
+		public int customer_no { get; set; }
+		public string vendor_no { get; set; }
+		
+	}
+}
---
+++ SP_API/Models/Packages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SP_API.Models
+{
+	class Packages
+	{
+		public int price { get; set; }
+		public string detail { get; set; }
+		public int start_date { get; set; }
+		public string image { get; set; }
+		public string email { get; set; }
+		public string end_date { get; set; }
+		public int eackage_no { get; set; }
+	}
+}
