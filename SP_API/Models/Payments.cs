b3fec5d2853ecc1798046e278b031b9f53edfcd3
Nirmani-Rathnayaka <nirmaninipunika053@gmail.com>
2020-11-20 13:39:13 +0530

    create models

---
+++ SP_API/Models/Payments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SP_API.Models
+{
+    public class Payments
+    {
+
+        public int card_no { get; set; }
+        public string cardholder_name { get; set; }
+        public int exp_date { get; set; }
+        public int cvc { get; set; }  
+    }
+}
