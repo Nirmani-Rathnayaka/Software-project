57187aca091c2d4cf999dbd49b8da8ae94270a9d
Vijan Rajitha Thennakoon <eg173206@engug.ruh.ac.lk>
2020-11-20 13:15:00 +0530

    Create Models

---
+++ SP_API/Models/Client_Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SP_API.Models
+{
+    public class Client_Account
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
--- SP_API/Models/Vendor.cs
+++ SP_API/Models/Vendor.cs
@@ -7,5 +7,10 @@ namespace SP_API.Models
 {
     public class Vendor
     {
+        public int vender_ID { get; set; }
+        public string company_name { get; set; }
+        public int tele_no { get; set; }
+        public string company_catogary { get; set; }
+        public string email { get; set; }
     }
 }
